5135ba1ebb477c5494bd63dcbbc1bdcc7f77b833
Sybille Cordeau <sybillecordeau@gmail.com>
2019-10-01 15:53:09 +0200

    i18n parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 class ApplicationController < ActionController::Base
-
   before_action :set_artists_menu
+  before_action :set_locale
 
   def set_artists_menu
     @artists = Artist.all
     @artists_alpha = @artists.sort_by { |a| a.last_name.downcase}
   end
 
+  def set_locale
+    I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
+  end
+
   def default_url_options
     { host: ENV["HOST"] || "www.dame-jeanne-bordeaux.fr" }
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
   end
 
 end
