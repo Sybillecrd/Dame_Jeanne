11b22a90199f35ee0a3c3439aef05e3c0a701374
Sybille Cordeau <sybillecordeau@gmail.com>
2019-12-03 15:32:43 +0100

    posts controller + begin index view

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render "posts/new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /en/ do
     root to: 'artists#index'
     resources :artists, only: [:index, :show]
+    resources :posts, only: [:index, :show]
     get :about, to: "pages#about"
     get :shops, to: "pages#shops"
     get :contact, to: "pages#contact"
     get :legalpage, to: 'pages#legalpage'
-    get :posts, to: "pages#posts"
   end
 end
