6ca33ee5e03f46f8539a2e5d87b98d66043c26f7
Sybille Cordeau <sybillecordeau@gmail.com>
2019-12-10 14:42:44 +0100

    media queries post show

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,8 +1,4 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   storage :file
-
-  process eager: true  # Force version generation at upload time.
-
-  process convert: 'jpg'
 end
