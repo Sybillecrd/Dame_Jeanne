27679e2a02c6b20a7ee2673630da4ffb8ac8ecdf
Sybille Cordeau <sybillecordeau@gmail.com>
2019-12-10 11:48:48 +0100

    fix update problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,7 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
   end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,8 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   storage :file
+
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
 end
